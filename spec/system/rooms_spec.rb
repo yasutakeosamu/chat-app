1a1abe5b9a474e9d0f119ad8a08cc1d0502ca9a5
yasutakeosamu <036yasutake@gmail.com>
2020-08-24 23:52:17 +0900

    結合テストの実装

---
+++ spec/system/rooms_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "チャットルームの削除機能", type: :system do
+  before do
+    @room_user = FactoryBot.create(:room_user)
+  end
+  it 'チャットルームを削除すると、関連するメッセージが全て削除されていること' do
+    # サインインする
+    sign_in(@room_user.user)
+    # 作成されたチャットルームへ遷移する
+    click_on(@room_user.room.name)
+    # メッセージ情報を5つDBに追加する
+    5.times do
+      fill_in 'message_content', with: FactoryBot.create(:message)
+      click_on 'commit'
+    end
+    # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを確認する
+    expect{
+      click_on 'チャットを終了する'
+    }.to change{Message.count}.by(-5)
+    # トップページに遷移していることを確認する
+    expect(current_path).to eq root_path
+  end
+end
